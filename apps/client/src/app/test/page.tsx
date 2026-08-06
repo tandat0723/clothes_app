91bb4198783bb198ac19a6e4f5d2a8aa825c7eb3
Tran Tan Dat <trantandat0723@gmail.com>
2026-08-06 19:04:06 +0700

    #14 feat(product-service): add authentication middleware and test endpoint

---
+++ apps/client/src/app/test/page.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@clerk/nextjs/server";
+
+const TestPage = async () => {
+  const { getToken } = await auth();
+  const token = await getToken();
+  console.log(token);
+
+  const res = await fetch("http://localhost:8000/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await res.json();
+  console.log(data);
+
+  return <div className="">TestPage</div>;
+};
+
+export default TestPage;
--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { clerkMiddleware, getAuth } from "@clerk/express";
 
 const app = express();
 app.use(
@@ -9,12 +10,24 @@ app.use(
   }),
 );
 
+app.use(clerkMiddleware());
+
 app.get("/health", (req: Request, res: Response) => {
   return res.status(200).json({
     status: "ok",
     uptime: process.uptime(),
     timestamp: Date.now(),
   });
+});
+
+app.get("/test", (req, res) => {
+  const auth = getAuth(req);
+  const userId = auth.userId;
+
+  if (!userId) {
+    return res.status(401).json({ message: "you are not logged in!" });
+  }
+  res.json({ message: "product service authenticated!" });
 });
 
 app.listen(8000, () => {
